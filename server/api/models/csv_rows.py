00e911b2220a319ad4e9186e9c9cbbba8916f0d0
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-12-13 23:59:54 -0800

    adding table for storing  individual row of csv file

---
+++ server/api/models/csv_rows.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.functions import func
+from sqlalchemy.sql.schema import Column, ForeignKey
+from sqlalchemy.sql.sqltypes import BigInteger, Boolean, DateTime, Integer
+
+from api.database import Base
+
+
+class CsvRows(Base):
+    """Contains individual rows of CSV"""
+
+    __tablename__ = "csv_rows"
+
+    id = Column(BigInteger, primary_key=True, autoincrement=True)
+    csv_id = Column(BigInteger, ForeignKey("csv_metadata.id"))
+    # Not sure if needed or if should just delete from table
+    processed = Column(Boolean, nullable=False, default=False)
+
+    prospect = relationship("CsvMetadata", foreign_keys=[csv_id])

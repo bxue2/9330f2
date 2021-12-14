22840a946b293e9bf7f190dcfe06d5265f085e37
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-12-14 00:00:38 -0800

    forgot column for row data for csv_rows table

--- server/api/models/csv_rows.py
+++ server/api/models/csv_rows.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql.functions import func
 from sqlalchemy.sql.schema import Column, ForeignKey
-from sqlalchemy.sql.sqltypes import BigInteger, Boolean, DateTime, Integer
+from sqlalchemy.sql.sqltypes import BigInteger, Boolean, DateTime, Integer, String
 
 from api.database import Base
 
@@ -14,6 +14,7 @@ class CsvRows(Base):
     id = Column(BigInteger, primary_key=True, autoincrement=True)
     csv_id = Column(BigInteger, ForeignKey("csv_metadata.id"))
     # Not sure if needed or if should just delete from table
+    csv_string = Column(String, nullable=False)
     processed = Column(Boolean, nullable=False, default=False)
 
     prospect = relationship("CsvMetadata", foreign_keys=[csv_id])

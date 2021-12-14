cb121158e602ffc763edb6bd80513b73ea9f6e1f
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-12-13 23:29:02 -0800

    changing csv table, removing success/fail

--- server/api/models/csv_metadata.py
+++ server/api/models/csv_metadata.py
@@ -13,6 +13,6 @@ class CsvMetadata(Base):
 
     id = Column(BigInteger, primary_key=True, autoincrement=True)
     total_rows = Column(Integer, nullable=False)
-    success = Column(Integer, nullable=False)
-    failed = Column(Integer, nullable=False)
+    processed = Column(Integer, nullable=False)
+    #Not sure if completed needed, could just compare processed and total_rows
     completed = Column(Boolean, nullable=False)

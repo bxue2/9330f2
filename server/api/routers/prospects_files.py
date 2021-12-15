a1c198a4d6514dd1177ffa729225e32f53e9bfd7
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-12-14 21:48:52 -0800

    fixing id path parameter syntax

--- server/api/routers/prospects_files.py
+++ server/api/routers/prospects_files.py
@@ -12,8 +12,6 @@ router = APIRouter(prefix="/api", tags=["prospects_files"])
 @router.post("/prospects_files", response_model=schemas.ProspectResponse)
 def upload_prospects_csv(
     current_user: schemas.User = Depends(get_current_user),
-    page: int = DEFAULT_PAGE,
-    page_size: int = DEFAULT_PAGE_SIZE,
     db: Session = Depends(get_db),
 ):
     """Verify User"""
@@ -31,11 +29,10 @@ def upload_prospects_csv(
     return {"id": 1, "row1": "row1", "row2": "row2"}
 
 #2
-@router.post("/prospects_files/:id/prospects", response_model=schemas.ProspectResponse)
+@router.post("/prospects_files/{id}/prospects", response_model=schemas.ProspectResponse)
 def import_csv(
     current_user: schemas.User = Depends(get_current_user),
-    page: int = DEFAULT_PAGE,
-    page_size: int = DEFAULT_PAGE_SIZE,
+    id: int,
     db: Session = Depends(get_db),
 ):
     """Verify User"""
@@ -44,20 +41,20 @@ def import_csv(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="Please log in"
         )
     # Step 1: Parse request to get column names
+    print(id)
     prospects = ProspectCrud.get_users_prospects(db, current_user.id, page, page_size)
     total = ProspectCrud.get_user_prospects_total(db, current_user.id)
 
     #Step 2: Start uploading, want this to happen async though
 
-    # Step 3: Return id number of csv in table so we know what to track?
+    # Step 3: Return id number of csv in table so we know what to track? doc mentions prospectsfile object?
     return {"csvid": 1}
 
 #3
-@router.get("/prospects_files/:id/progress", response_model=schemas.ProspectResponse)
+@router.get("/prospects_files/{id}/progress", response_model=schemas.ProspectResponse)
 def get_upload_status(
     current_user: schemas.User = Depends(get_current_user),
-    page: int = DEFAULT_PAGE,
-    page_size: int = DEFAULT_PAGE_SIZE,
+    id: int,
     db: Session = Depends(get_db),
 ):
     """Verify User"""

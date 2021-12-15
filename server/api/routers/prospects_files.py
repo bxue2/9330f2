c59ed93c49b5c1425214a19cb2b62c55b040b19b
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-12-14 20:54:17 -0800

    changing route names to match doc

---
+++ server/api/routers/prospects_files.py
@@ -0,0 +1,71 @@
+from fastapi import APIRouter, HTTPException, status, Depends
+from sqlalchemy.orm.session import Session
+from api import schemas
+from api.dependencies.auth import get_current_user
+from api.core.constants import DEFAULT_PAGE, DEFAULT_PAGE_SIZE
+from api.crud import ProspectCrud
+from api.dependencies.db import get_db
+
+router = APIRouter(prefix="/api", tags=["prospects_files"])
+
+#1
+@router.post("/prospects_files", response_model=schemas.ProspectResponse)
+def upload_prospects_csv(
+    current_user: schemas.User = Depends(get_current_user),
+    page: int = DEFAULT_PAGE,
+    page_size: int = DEFAULT_PAGE_SIZE,
+    db: Session = Depends(get_db),
+):
+    """Verify User"""
+    if not current_user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Please log in"
+        )
+
+    #Step 1: Store CSV file somehow?
+    # Maybe add new db table containing each row of csv
+    # Going to use current DB, but might make sense to use a different storage method
+
+
+    #Step 2: Need to return sample data for column matching later if successful upload
+    return {"row1": "row1", "row2": "row2"}
+
+#2
+@router.post("/prospects_files/:id/prospects", response_model=schemas.ProspectResponse)
+def import_csv(
+    current_user: schemas.User = Depends(get_current_user),
+    page: int = DEFAULT_PAGE,
+    page_size: int = DEFAULT_PAGE_SIZE,
+    db: Session = Depends(get_db),
+):
+    """Verify User"""
+    if not current_user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Please log in"
+        )
+    # Step 1: Parse request to get column names
+    prospects = ProspectCrud.get_users_prospects(db, current_user.id, page, page_size)
+    total = ProspectCrud.get_user_prospects_total(db, current_user.id)
+
+    #Step 2: Start uploading, want this to happen async though
+
+    # Step 3: Return id number of csv in table so we know what to track?
+    return {"csvid": 1}
+
+#3
+@router.get("/prospects_files/:id/progress", response_model=schemas.ProspectResponse)
+def get_upload_status(
+    current_user: schemas.User = Depends(get_current_user),
+    page: int = DEFAULT_PAGE,
+    page_size: int = DEFAULT_PAGE_SIZE,
+    db: Session = Depends(get_db),
+):
+    """Verify User"""
+    if not current_user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Please log in"
+        )
+
+    # Check csv_metadata table to get completed status/progress, then return info
+
+    return {"completed": True, "total": 1, "processed": 1}

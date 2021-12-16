f111cc9806df800c1f9f9ebee95afd90a3ab6100
bxue2 <1082507+bxue2@users.noreply.github.com>
2021-12-15 19:56:26 -0800

    commenting out user requirement for route 1 temporarily

--- server/api/routers/prospects_files.py
+++ server/api/routers/prospects_files.py
@@ -1,5 +1,4 @@
-from fastapi import APIRouter, HTTPException, status, Depends
-from fastapi.param_functions import File
+from fastapi import APIRouter, HTTPException, status, Depends, File, UploadFile
 from pydantic.main import BaseModel
 from sqlalchemy.orm.session import Session
 from api import schemas
@@ -8,10 +7,6 @@ from api.crud import ProspectCrud
 from api.dependencies.db import get_db
 
 router = APIRouter(prefix="/api", tags=["prospects_files"])
-
-#Request body for route 1
-class CSVFile(BaseModel):
-    file: File
 
 #Request body for route 2
 class CSVHeaders(BaseModel):
@@ -24,20 +19,19 @@ class CSVHeaders(BaseModel):
 #1
 @router.post("/prospects_files", response_model=schemas.ProspectsFileUpload)
 def upload_prospects_csv(
-    current_user: schemas.User = Depends(get_current_user),
-    db: Session = Depends(get_db),
-    csv: CSVFile
+    # current_user: schemas.User = Depends(get_current_user),
+    file: UploadFile = File(...)
 ):
-    """Verify User"""
-    if not current_user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="Please log in"
-        )
+    # """Verify User"""
+    # if not current_user:
+    #     raise HTTPException(
+    #         status_code=status.HTTP_401_UNAUTHORIZED, detail="Please log in"
+    #     )
 
     #Step 1: Store CSV file somehow?
     # Create entry in DB first
     # Going to add a local folder to store csv files, rename based on db id
-    
+    # file.
 
     #Step 2: Need to return sample data for column matching later if successful upload plus id of csv
     return {"id": 1, "rows": "row1"}
